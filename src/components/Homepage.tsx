ce5a28bc5e6ffc14859086964adf2dd150c21f82
Meghan <meghan.woodruff@cune.org>
2025-06-22 09:31:51 -0500

    changed wording on the homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,13 +4,21 @@ export const Homepage = () => {
   return (
     <Box
       sx={{
-        display: "flex",
+        //display: "flex",
         margin: "1rem",
-        flex: 1,
+        // flex: 1,
       }}
     >
-      <Typography variant="h4">
-        Welcome to the 2025 Youth Gathering Seat Viewer application!
+      <Typography variant="h4" sx={{ mb: "2rem" }}>
+        2025 Youth Gathering Seat Viewer Application
+      </Typography>
+      <Typography variant="h6" sx={{ mb: "2rem" }}>
+        To view your Mass Event seat assignments, click "Seat Assignments by
+        Congregation" and type in your Congregation ID.
+      </Typography>
+      <Typography variant="h6">
+        To see how each Dome section is organized and the seat
+        types (like ADA), click "Sections with Seat Type"
       </Typography>
     </Box>
   );
